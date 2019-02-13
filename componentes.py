9722502e7b0459636e93b1f7fa02998d65b405e4
Gustavo Restrepo <gustavo.restrepo@utp.edu.co>
2019-02-13 16:47:31 -0500

    recortando sprites-creacion del jugador y enemigos

---
+++ componentes.py
@@ -0,0 +1,32 @@
+import pygame as pg
+import sys, os
+
+
+BLANCO = [250, 250, 250]
+NEGRO = [0,0,0]
+BLANCO = (255,255,255)
+VERDE = (0,255,0)
+ROJO = (255,0,0)
+AZUL = (0,0,255)
+
+def recortar(imagen, ancho, alto, fil, col):
+    l1 = []
+    l2 = []
+    for i in range(0, fil):
+        for j in range(0, col):
+            cuadro = imagen.subsurface(j*ancho, alto*i, ancho, alto)
+            l2.append(cuadro)
+        l1.append(l2)
+        l2 = []
+    return l1
+
+
+
+def spritegoku():
+    gok = {'standby': recortar(pg.image.load('./resource/goku/standby.png'), 23, 32, 2, 1),
+            'walk': recortar(pg.image.load('./resource/goku/walk.png'), 24, 31, 2, 4),
+            'golpe1': recortar(pg.image.load('./resource/goku/golpe1.png'), 26, 30, 2, 3),
+            'golpe2': recortar(pg.image.load('./resource/goku/golpe2.png'), 24, 31, 2, 4),
+            'kame': recortar(pg.image.load('./resource/goku/kame.png'), 25, 32, 2, 3),
+            'morir': recortar(pg.image.load('./resource/goku/morir.png'), 31, 32, 2, 4)}
+    return gok
--- menu.py
+++ menu.py
@@ -1,10 +1,11 @@
 import pygame as pg
+from componentes import *
 import sys, os
 
 if sys.platform in ["win32","win64"]: os.environ["SDL_VIDEO_CENTERED"]="1"
 
 BLANCO = [250, 250, 250]
-NEGRO = (0,0,0)
+NEGRO = [0,0,0]
 BLANCO = (255,255,255)
 VERDE = (0,255,0)
 ROJO = (255,0,0)
@@ -19,16 +20,14 @@ class Pant(pg.sprite.Sprite):
         self.pantalla = pg.display.set_mode([self.ancho, self.alto])
         pg.display.set_caption("DBZ")
 
-
-
     def main(self):
-        img_gok = pg.image.load('./resource/goku/goku-sprites.png')
-        m = recortar(img_gok, 6, 4)
+        # img_gok = pg.image.load('./resource/goku/goku-sprites.png')
+        m = spritegoku()
         goku = Personaje(m, 660, 620)
         jugadores = pg.sprite.Group()
         jugadores.add(goku)
         reloj = pg.time.Clock()
-        rl = 11
+        rl = 8
         fin = False
         while not fin:
             for event in pg.event.get():
@@ -45,26 +44,43 @@ class Pant(pg.sprite.Sprite):
                     pg.quit()
                     sys.exit()
                 if event.type == pg.KEYDOWN:
-                    if event.key == pg.K_LEFT:
-                        goku.accion = 3
-                        goku.con = 0
-                        goku.vel_x = -5
-                        rl = 7.6
                     if event.key == pg.K_RIGHT:
-                        goku.accion = 2
+                        goku.accion = 'walk'
+                        goku.dir = 0
                         goku.con = 0
-                        goku.vel_x = 5
-                        rl = 7.6
-                if event.type == pg.KEYUP:
+                        goku.vel_x = 6
+                        rl = 8.7
+
                     if event.key == pg.K_LEFT:
-                        goku.accion = 1
+                        goku.accion = 'walk'
+                        goku.dir = 1
+                        goku.con = 0
+                        goku.vel_x = -6
+                        rl = 8.7
+
+                    if event.key == pg.K_a:
+                        goku.accion = 'golpe2'
                         goku.con = 0
                         goku.vel_x = 0
-                    if event.key == pg.K_LEFT:
-                        goku.accion = 0
+                        rl = 8
+
+                if event.type == pg.KEYUP:
+                    if event.key == pg.K_RIGHT:
+                        goku.accion = 'standby'
+                        goku.dir = 0
                         goku.con = 0
                         goku.vel_x = 0
 
+                    if event.key == pg.K_LEFT:
+                        goku.accion = 'standby'
+                        goku.dir = 1
+                        goku.con = 0
+                        goku.vel_x = 0
+
+                    # if event.key == pg.K_a:
+                    #     goku.accion = 'standby'
+                    #     goku.con = 0
+                    #     goku.vel_x = 0
 
 
             jugadores.update()
@@ -78,9 +94,11 @@ class Personaje(pg.sprite.Sprite):
         pg.sprite.Sprite.__init__(self)
         self.m = m
         self.con = 0
-        self.accion = 0
-        self.lim = 3
-        self.image = self.m[self.accion][self.con]
+        self.accion = 'standby'
+        self.dir = 0
+        self.lim = len(self.m[self.accion][self.dir])-1
+        self.lis = self.m[self.accion]
+        self.image = self.lis[self.dir][self.con]
         self.rect = self.image.get_rect()
         self.rect.x = (ancho/2)-16
         self.rect.y = (alto/2)-16
@@ -89,28 +107,43 @@ class Personaje(pg.sprite.Sprite):
         self.objs = pg.sprite.Group()
 
     def update(self):
-        if self.con < 3:
-            self.con +=1
-        else:
-            self.con = 0
+        self.lim = len(self.m[self.accion][self.dir])-1
 
-        self.image = pg.transform.scale(self.m[self.accion][self.con], [80,80])
+        if (self.accion != 'golpe2') or (self.accion != 'morir') or (self.accion != 'golpe1') or (self.accion != 'kame'):
+            if self.con < self.lim:
+                self.con +=1
+            else:
+                self.con = 0
+
+        if (self.accion == 'golpe2') or (self.accion == 'morir') or (self.accion == 'golpe1') or (self.accion == 'kame'):
+            if self.con < self.lim:
+                self.con +=1
+            else:
+                self.con = 0
+                self.accion = 'standby'
+
+        self.lis = self.m[self.accion]
+        # self.image = self.lis[self.dir][self.con]
+        self.image = pg.transform.scale(self.lis[self.dir][self.con], [self.rect.width * 3,self.rect.height * 3])
+        print('con: ',self.con)
+        print('lim: ',self.lim)
         self.rect.x += self.vel_x
         self.rect.y += self.vel_y
 
 
-def recortar(imagen, alto, ancho):
-    l1 = []
-    l2 = []
-    for i in range(0, alto):
-        for j in range(0, ancho):
-            cuadro = imagen.subsurface(j*24, 32*i, 24, 32)
-            l2.append(cuadro)
-        l1.append(l2)
-        l2 = []
-    return l1
+# def recortar(imagen, alto, ancho):
+#     l1 = []
+#     l2 = []
+#     for i in range(0, alto):
+#         for j in range(0, ancho):
+#             cuadro = imagen.subsurface(j*24, 32*i, 24, 32)
+#             l2.append(cuadro)
+#         l1.append(l2)
+#         l2 = []
+#     return l1
 
 if __name__ == '__main__':
+    pg.init()
     print("Hola")
 
 
